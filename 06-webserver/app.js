1990560f5a51a528cb6c7607650b4b47611469b4
Javier Lucero <javimcl1986@gmail.com>
2022-06-29 14:48:27 -0500

    used HBS

---
+++ 06-webserver/app.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const hbs = require('hbs');
+
+const port = 8080
+
+//Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+//static content
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'Javier Lucero',
+        titulo:'Curso Node'
+    })
+})
+
+app.get('/hola-mundo', (req, res) => {
+    res.send('Hello World on their respective route')
+})
+app.get('/generic', (req, res) => {
+    res.render('generic', {
+        nombre: 'Javier Lucero',
+        titulo:'Curso Node'
+    })
+})
+
+app.get('/elements', (req, res) => {
+    res.render('elements', {
+        nombre: 'Javier Lucero',
+        titulo:'Curso Node'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.send('404 | Page not found')
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
